b8c9572376ad49849e9b58e745b0f91d7c3dd193
ADappts <129835970+BearToRear@users.noreply.github.com>
2024-01-26 19:48:39 +0530

    Add files via upload

---
+++ SelfII/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1937620.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 164785809, 164993570 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/164491659/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/164491660/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/164491658/1/ammo.js', 'preload' : true},
+];
